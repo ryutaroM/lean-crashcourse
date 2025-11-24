28e9fe0cf6d815f22eb11d35aba6f66a43cf15a9
ryutaroM <arcticmonkey326@gmail.com>
2025-11-24 14:39:47 +0900

    feat: implement MyNat.ofNat function and add instances for OfNat and Add

--- CrashCourse/TypeClass.lean
+++ CrashCourse/TypeClass.lean
@@ -1,1 +1,31 @@
 import CrashCourse.NaturalNumber
+
+def MyNat.ofNat(n:Nat):MyNat :=
+  match n with
+    | 0 => MyNat.zero
+    | n + 1 => MyNat.succ (MyNat.ofNat n)
+
+@[default_instance]
+instance (n : Nat) : OfNat MyNat n where
+  ofNat := MyNat.ofNat n
+
+#check 0
+#check 1
+
+instance : Add MyNat where
+  add := MyNat.add
+
+#check 1 + 1
+#check MyNat.zero + MyNat.one
+
+example : 1 = MyNat.one := by
+  rfl
+
+example : 2 = MyNat.two := by
+  rfl
+
+example : 1 + 1 = 2 := by
+  rfl
+
+example (n : MyNat) : n + 0 = n := by
+  rfl
